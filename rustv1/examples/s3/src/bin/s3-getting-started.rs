3ef259d6c84a7a2e0e1822076918269c34791cbd
Ronald Ramon Ilma Campana <rilma@Ronalds-MacBook-Pro.local>
2025-02-24 11:21:52 +0100

    conditioning for eu-north-1

--- rustv1/examples/s3/src/bin/s3-getting-started.rs
+++ rustv1/examples/s3/src/bin/s3-getting-started.rs
@@ -19,12 +19,12 @@ use uuid::Uuid;
 
 #[tokio::main]
 async fn main() -> Result<(), S3ExampleError> {
-    let region_provider = RegionProviderChain::first_try(Region::new("us-west-2"));
+    let region_provider = RegionProviderChain::first_try(Region::new("eu-north-1"));
     let region = region_provider.region().await.unwrap();
     let shared_config = aws_config::from_env().region(region_provider).load().await;
     let client = Client::new(&shared_config);
-    let bucket_name = format!("amzn-s3-demo-bucket-{}", Uuid::new_v4());
-    let file_name = "s3/testfile.txt".to_string();
+    let bucket_name = format!("{}-s3-demo-bucket-{}", region, Uuid::new_v4());
+    let file_name = "testfile.txt".to_string();
     let key = "test file key name".to_string();
     let target_key = "target_key".to_string();
 
@@ -47,7 +47,7 @@ async fn run_s3_operations(
     s3_code_examples::create_bucket(&client, &bucket_name, &region).await?;
     let run_example: Result<(), S3ExampleError> = (async {
         s3_code_examples::upload_object(&client, &bucket_name, &file_name, &key).await?;
-        let _object = s3_code_examples::download_object(&client, &bucket_name, &key).await;
+        // let _object = s3_code_examples::download_object(&client, &bucket_name, &key).await;
         s3_code_examples::copy_object(&client, &bucket_name, &bucket_name, &key, &target_key)
             .await?;
         s3_code_examples::list_objects(&client, &bucket_name).await?;
